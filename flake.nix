d1404fe01bf7ec0a7f1f0c6b68a9d888a17475df
Fay Carsons <faycarsons23@gmail.com>
2025-10-20 09:11:01 -0400

    setup

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Rig development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              pkg-config
+              cmake
+              openssl
+              rustToolchain
+            ];
+          };
+      }
+    );
+}
